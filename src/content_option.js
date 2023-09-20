48cb28e320c3af828c650bf0223ef8dd59ef8f74
Abdurahman Jafer <95960560+its0beno@users.noreply.github.com>
2023-09-20 10:49:05 +0300

    inital commit

---
+++ src/content_option.js
@@ -0,0 +1,193 @@
+const logotext = "BENO";
+const meta = {
+  title: "Abdurahman",
+  description:
+    "I’m Abdurahman Jafer Software Enginner _ Full stack devloper,currently working in AA",
+};
+
+const introdata = {
+  title: "I’m Abdurahman Jafer",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop web apps",
+  },
+  description: `Driving Digital Transformation: Bringing Your Vision to Life with Passion and
+  Expertise!`,
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme: `A skilled and dedicated full-stack developer with 2+ years of expertise in Python
+    Django and Node.js Express. Proficient in building and optimizing web
+    applications, I excels in developing RESTful APIs and delivering seamless user
+    experiences. With a strong problem-solving mindset and attention to detail, I
+    am committed to creating high-quality software solutions that drive innovation
+    and exceed client expectations.`,
+};
+const worktimeline = [
+  {
+    jobtitle: "Full Stack Developer",
+    where: "Skylink Ict",
+    date: "2022",
+  },
+  {
+    jobtitle: "Backend Developer",
+    where: "Full Circle Solutions LLC",
+    date: "2023",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 90,
+  },
+  {
+    name: "Djano",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 70,
+  },
+  {
+    name: "Angular",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "API Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Deployment",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Testing",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "Rental Managment Stack: Django, HTML, CSS, Bootstrap, Javascript, jQuery, MYSQL, AWS",
+    link: "http://django-env.eba-nj9f65re.us-west-2.elasticbeanstalk.com/",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "E-learning Stack : Django, HTML, CSS, Bootstrap, Javascript,  MYSQL, AWS",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "Hospital Management System Stack: Django, HTML, CSS, Bootstrap, Javascript,  MYSQL, AWS",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "Collection Management System Stack :NodeJs, Angular, HTML, CSS, Bootstrap, TypeScript,  MYSQL, AWS",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "ERP System for Construction Companies Stack: Django, React, Typescript, jQuery, MSSQL, AWS",
+    link: "#",
+  },
+  // {
+  //   img: "https://picsum.photos/400/700/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+
+  // {
+  //   img: "https://picsum.photos/400/600/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/300/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/550/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/700/?grayscale",
+  //   description:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "boodebeno@gmail.com",
+  YOUR_FONE: "+251 988174455",
+  description:
+    "I am not actively looking for a new role but I am open for discussion. Please feel free to contact me.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_woyu1id",
+  YOUR_TEMPLATE_ID: "template_77d2itl",
+  YOUR_USER_ID: "SB_1Ki6LP6fGq1eHt",
+};
+
+const socialprofils = {
+  github: "https://github.com/its0beno",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com/in/its0beno",
+  twitter: "https://twitter.com/its0beno",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
